eed3f632292d7001522e39ff6c96b06a99e07183
Your Name <you@example.com>
2025-04-06 10:25:46 +0000

    9-binary_tree_height

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /* Provided print function (only for testing) */
 void binary_tree_print(const binary_tree_t *);
 
