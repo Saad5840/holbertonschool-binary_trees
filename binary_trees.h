ba7188008b38163b74b7f3e7ae44fea4403f0cb4
Your Name <you@example.com>
2025-04-06 10:16:36 +0000

    7-binary_tree_inorder

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,8 @@ int binary_tree_is_root(const binary_tree_t *node);
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
 /* Provided print function (only for testing) */
 void binary_tree_print(const binary_tree_t *);
 
